a2b0d9cf218b48c29b633ad5f694765817bdaf25
CarloshDuocUC <caa.hernandeza@duocuc.cl>
2024-07-06 04:12:38 +0000

    Gestión de claves y base de datos SQL

--- integrantes.py
+++ integrantes.py
@@ -1,3 +1,5 @@
+print("Nombres de Integrantes Examen-Transversal-Programación-y-Redes-Virtualizadas-DRY7122")
+print("")
 
 integrantes = ["Sebastian Aviles", "Salomon Vergara", "Carlos Hernandez"]
 
